e047c08275ac3b60febec23c175100d4fef42d92
anne-urai <anne.urai@gmail.com>
2017-07-23 21:05:28 +0200

    rerun

--- a0_compileMe.m
+++ a0_compileMe.m
@@ -42,9 +42,9 @@ function [] = compileMe(fname)
     % parameter file for HDDM
     % ============================================ #
 
-    nsmp       = [5000]
-    datasets   = [0:6]; % RT-RDK and MEG-PL
-    models     = [7:11]; % the nr of the models
+    nsmp       = [10000]
+    datasets   = [1:2]; % RT-RDK and MEG-PL
+    models     = [4:5]; % the nr of the models
     nrTraces   = 15; % nr of chains, 15 cores/node (so make sure this is a multiple of 15)
 
     alldat = [];
--- e0_plotAll.m
+++ e0_plotAll.m
@@ -31,9 +31,11 @@ datasetnames = { {'2AFC, RT', 'n = 22'}, ...
 disp('starting');
 e8_serialBias_SfN_PPC; % figure 2, show that all models fit OK
 
-assert(1==0);
-f0_schematic_DDM_bias; % figure 3a
+%f0_schematic_DDM_bias; % figure 3a
 e1_serialBias_SfN_DIC; % figure 3b & c
+
+assert(1==0);
+
 e1b_serialBias_SfN_ModelFreeCorrelation; % figure 4
 e3_serialBias_SfN_Posteriors; % figure 4c
 e9_prevCorrect; % error vs correct
