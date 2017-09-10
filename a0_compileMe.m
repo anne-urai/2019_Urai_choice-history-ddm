f8442888ab97e171829282cbe752ac13d480b0b9
anne-urai <anne.urai@gmail.com>
2017-09-10 23:05:39 +0200

    removed prevstim from regression models

--- a0_compileMe.m
+++ a0_compileMe.m
@@ -44,7 +44,7 @@ function [] = compileMe(fname)
     % ============================================ #
 
     nsmp       = [10000]
-    datasets   = [0:8]; % RT-RDK and MEG-PL
+    datasets   = [1]; % RT-RDK and MEG-PL
     models     = [12]; % the nr of the models
     nrTraces   = 15; % nr of chains, 15 cores/node (so make sure this is a multiple of 15)
 
