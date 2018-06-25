ad0c80e95e4f47722b9f3727eae0ed90c1a444e3
Anne Urai <anne.urai@gmail.com>
2018-06-25 10:36:48 -0400

    multiplicative

--- multiplicative_vbias.m
+++ multiplicative_vbias.m
@@ -41,46 +41,16 @@ for d = [4 5] % only NatComm and Anke_MEG_neutral, with varying coherence level
         allresults(c).v_prevresp        = results.(sprintf('dc_0_%s_1__stimcodingdczprevrespmultiplicative', cohs{c})) - ...
             results.(sprintf('dc_0_%s_2__stimcodingdczprevrespmultiplicative', cohs{c}));
         thiscoh = num2str(cohlevels(c));
+        if d == 4,
+            thiscoh = num2str(cohlevels(c) * 100);
+        end
         allresults(c).criterionshift    = results.(['repetition_c' regexprep(thiscoh, '\.', '\_')]);
         allresults(c).subjnr            = results.subjnr;
         allresults(c).marker 			= 'o';
         allresults(c).meancolor 		= 0.9*scattercols(c, :);
         allresults(c).scattercolor	 	= scattercols(c, :);
     end
-    
-    
-%     sp2 = subplot(3,3,1); hold on;
-%     [rho2, tt2, handles] = plotScatter(allresults, 'v_prevresp', 0.05, 0);
-%     set(gca, 'yticklabel', []);
-%     ylabel('P(repeat)');
-%     
-%     % move together
-%     ss = suplabel(cat(2, datasetnames{d}{1}, ' ', datasetnames{d}{2}), 't');
-%     
-%     set(ss, 'fontweight', 'normal');
-%     ss.FontWeight = 'normal';
-%     ss.Position(2) = ss.Position(2) - 0.03;
-%     
-%     % add colored axes after suplabel (which makes them black)
-%     xlabel(sp2, 'History shift in v_{bias}');
-%     set(sp2, 'xcolor', colors(2, :), 'ycolor', 'k');
-%     
-%     %% make another subplot with the correlation coefficients!
-%     subplot(4,4,2); clear rho pval; hold on;
-%     for c = 1:length(cohs),
-%         [rho(c), pval(c)] = corr(allresults(c).v_prevresp, allresults(c).criterionshift, ...
-%             'rows', 'complete');
-%     end
-%     
-%     scatter(cohlevels(pval < 0.05), rho(pval < 0.05), 20, scattercols((pval < 0.05), :), 'filled');
-%     scatter(cohlevels(pval > 0.05), rho(pval > 0.05), 18, scattercols((pval > 0.05), :));
-%     
-%     set(gca, 'xtick', cohlevels);
-%     ylabel('Correlation coefficient');
-%     xlabel('Coherence level');
-%     offsetAxes;
-% 
-%     
+      
     %% FLIP AROUND THE BIAS TERM FOR ALTERNATORS, THEN PLOT ITS MAGNITUDE AS A FUNCTION OF COHERENCE LEVELS
     
     subplot(3,3,d);
