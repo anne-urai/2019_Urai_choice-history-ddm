7e75a0d4838088554062c8127f862e6f72b898e8
Jan Willem de Gee <jwdegee@jans-mbp.ad.bcm.edu>
2018-10-02 13:05:15 -0500

    final DDM and OU simulations

--- simulations/3_lca_rts.py
+++ simulations/3_lca_rts.py
@@ -11,7 +11,7 @@ import seaborn as sns
 import pandas as pd
 from IPython import embed as shell
 
-from sim_tools import LCA_traces_get, two_accumulater_traces_apply_bounds
+from sim_tools import get_LCA_traces, apply_bounds_accumulater_traces
 from sim_tools import summary_plot, conditional_response_plot, traces_plot
 
 sns.set(style='ticks', font='Arial', font_scale=1, rc={
@@ -29,16 +29,6 @@ sns.set(style='ticks', font='Arial', font_scale=1, rc={
     'ytick.color':'Black',} )
 sns.plotting_context()
 
-# data_folder = '/home/degee/research/model_simulations/lca_data/'
-# fig_folder = '/home/degee/research/model_simulations/lca_figs/'
-data_folder = '/Users/janwillem/Desktop/simulations/lca_data/'
-fig_folder = '/Users/janwillem/Desktop/simulations/lca_figs/'
-
-t = 1000
-dt = 1
-timesteps = int(t/dt)
-print(timesteps)
-    
 def do_simulations(params):
     
     rt = []
@@ -48,34 +38,40 @@ def do_simulations(params):
     x2 = []
     for stim in [1,0]:
         
-        # compute:
-        x1_dum, x2_dum = LCA_traces_get(v=params['v'],
+        # get traces:
+        x1_dum, x2_dum = get_LCA_traces(v=params['v'],
                                 k=params['k'],
                                 w=params['w'],
                                 dc=params['dc'],
                                 z=params['z'],
                                 pre_generated=False,
                                 stim=stim,
-                                nr_trials=params['nr_trials'],)
-        rt_dum, response_dum = two_accumulater_traces_apply_bounds(x1_dum, x2_dum, a=params['a'],)
+                                nr_trials=params['nr_trials'],
+                                tmax=tmax,
+                                dt=dt,)
+        
+        # apply bounds:
+        rt_dum, response_dum = apply_bounds_accumulater_traces(x1_dum, x2_dum, a=params['a'],)
 
-        # append:
+        # store results:
         x1.append(x1_dum)
         x2.append(x2_dum)
         rt.append(rt_dum)
         response.append(response_dum)
         stimulus.append(np.ones(params['nr_trials']) * stim)
-    
+
     # traces:
     x1 = pd.DataFrame(np.array(np.vstack(x1), dtype='float16')).iloc[:200,:]
     x2 = pd.DataFrame(np.array(np.vstack(x2), dtype='float16')).iloc[:200,:]
+    x1.columns = np.arange(0,tmax,dt)
+    x2.columns = np.arange(0,tmax,dt)
     if os.path.exists(os.path.join(data_folder, 'x1_{}.hdf'.format(params['subj_idx']))):
         os.remove(os.path.join(data_folder, 'x1_{}.hdf'.format(params['subj_idx'])))
     if os.path.exists(os.path.join(data_folder, 'x2_{}.hdf'.format(params['subj_idx']))):
         os.remove(os.path.join(data_folder, 'x2_{}.hdf'.format(params['subj_idx'])))
     x1.to_hdf(os.path.join(data_folder, 'x1_{}.hdf'.format(params['subj_idx'])), key='lca')
     x2.to_hdf(os.path.join(data_folder, 'x2_{}.hdf'.format(params['subj_idx'])), key='lca')
-
+    
     # rt dataframe:    
     df = pd.DataFrame()
     df.loc[:,'rt'] = (np.concatenate(rt) * dt) + ndt
@@ -85,17 +81,24 @@ def do_simulations(params):
     df.loc[:,'subj_idx'] = params['subj_idx']
     df.to_csv(os.path.join(data_folder, 'df_{}.csv'.format(params['subj_idx'])))
 
+data_folder = os.path.expanduser('~/Desktop/simulations/lca_data/')
+fig_folder = os.path.expanduser('~/Desktop/simulations/lca_figs/')
+   
 simulate = True
 nr_trials = int(1e5) #100K
-ndt = 15
+tmax = 5
+dt = 0.01
 
-v = 0.01
-k = 0.05
-w = 0.054 # neutral
-# w = 0.054 + 0.040 # inhibition dominant
-a = 1.5
-dc = 0.1
+v = 1.4
+a = 1.4
+k = 5
+w = 5.4 # leak dominant
+dc = 10
 z = 0
+ndt = 0.1
+
+a2 = 1.4 # inhibition dominant
+w2 = 9.4 # inhibition dominant
 
 sArray = [
     
@@ -103,37 +106,62 @@ sArray = [
     {'subj_idx':0, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
     {'subj_idx':1, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
     {'subj_idx':2, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
-
+    
     # LCA starting point bias:
     {'subj_idx':3, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z+0.1,z], 'nr_trials':nr_trials},
     {'subj_idx':4, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z+0.2,z], 'nr_trials':nr_trials},
     {'subj_idx':5, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z+0.3,z], 'nr_trials':nr_trials},
-
-    # LCA input bias:
-    {'subj_idx':6, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc+0.004,dc], 'z':[z,z], 'nr_trials':nr_trials},
-    {'subj_idx':7, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc+0.005,dc], 'z':[z,z], 'nr_trials':nr_trials},
-    {'subj_idx':8, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc+0.006,dc], 'z':[z,z], 'nr_trials':nr_trials},
-
-    # LCA leak bias:
-    {'subj_idx':9, 'v':[v,0], 'k':[k-0.002,k+0.002], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
-    {'subj_idx':10, 'v':[v,0], 'k':[k-0.003,k+0.003], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
-    {'subj_idx':11, 'v':[v,0], 'k':[k-0.004,k+0.004], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
-
-    # LCA inhibition bias:
-    {'subj_idx':12, 'v':[v,0], 'k':[k,k], 'w':[w+0.002,w-0.002], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
-    {'subj_idx':13, 'v':[v,0], 'k':[k,k], 'w':[w+0.003,w-0.003], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
-    {'subj_idx':14, 'v':[v,0], 'k':[k,k], 'w':[w+0.004,w-0.004], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
     
+    # LCA input bias:
+    {'subj_idx':6, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc+0.3,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    {'subj_idx':7, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc+0.6,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    {'subj_idx':8, 'v':[v,0], 'k':[k,k], 'w':[w,w], 'a':[a,a], 'dc':[dc+0.9,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    
+    # LCA leak bias:
+    {'subj_idx':9, 'v':[v,0], 'k':[k-0.2,k+0.2], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    {'subj_idx':10, 'v':[v,0], 'k':[k-0.4,k+0.4], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    {'subj_idx':11, 'v':[v,0], 'k':[k-0.6,k+0.6], 'w':[w,w], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    
+    # LCA inhibition bias:
+    {'subj_idx':12, 'v':[v,0], 'k':[k,k], 'w':[w+0.2,w-0.2], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    {'subj_idx':13, 'v':[v,0], 'k':[k,k], 'w':[w+0.4,w-0.4], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    {'subj_idx':14, 'v':[v,0], 'k':[k,k], 'w':[w+0.6,w-0.6], 'a':[a,a], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    
+    # # LCA neutral:
+    # {'subj_idx':15, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':16, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':17, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    
+    # # LCA starting point bias:
+    # {'subj_idx':18, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z+0.05,z], 'nr_trials':nr_trials},
+    # {'subj_idx':19, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z+0.15,z], 'nr_trials':nr_trials},
+    # {'subj_idx':20, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z+0.25,z], 'nr_trials':nr_trials},
+    
+    # # LCA input bias:
+    # {'subj_idx':21, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc+0.3,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':22, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc+0.6,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':23, 'v':[v,0], 'k':[k,k], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc+0.9,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    
+    # # LCA leak bias:
+    # {'subj_idx':24, 'v':[v,0], 'k':[k-0.2,k+0.2], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':25, 'v':[v,0], 'k':[k-0.5,k+0.5], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':26, 'v':[v,0], 'k':[k-0.8,k+0.8], 'w':[w2,w2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    
+    # # LCA inhibition bias:
+    # {'subj_idx':27, 'v':[v,0], 'k':[k,k], 'w':[w2+0.2,w2-0.2], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':28, 'v':[v,0], 'k':[k,k], 'w':[w2+0.5,w2-0.5], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+    # {'subj_idx':29, 'v':[v,0], 'k':[k,k], 'w':[w2+0.8,w2-0.8], 'a':[a2,a2], 'dc':[dc,dc], 'z':[z,z], 'nr_trials':nr_trials},
+
     ]
 
 if simulate:
     from joblib import Parallel, delayed
-    n_jobs = 3
+    n_jobs = 6
     res = Parallel(n_jobs=n_jobs)(delayed(do_simulations)(params) for params in sArray)
     # for params in sArray:
     #     do_simulations(params)
 
-groups = [[0,1,2], [3,4,5], [6,7,8], [9,10,11], [12,13,14],]
+groups = [[0,1,2], [3,4,5], [6,7,8], [9,10,11], [12,13,14],[15,16,17], [18,19,20], [21,22,23], [24,25,26], [27,28,29],]
 quantiles = [0, 0.1, 0.3, 0.5, 0.7, 0.9, 1]
 for i, group in enumerate(groups):
     
@@ -149,12 +177,12 @@ for i, group in enumerate(groups):
 
     # plot summary:
     quantiles = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
-    fig = conditional_response_plot(df, quantiles, mean_response)
+    fig = conditional_response_plot(df, quantiles, mean_response, xlim=(0.1,0.7))
     fig.savefig(os.path.join(fig_folder, 'crf_{}.pdf'.format(i)))
 
     # plot conditional response function:
     quantiles = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
-    fig = summary_plot(df, quantiles, mean_correct, mean_response)
+    fig = summary_plot(df, quantiles, mean_correct, mean_response, xlim=(0.1,0.7))
     fig.savefig(os.path.join(fig_folder, 'summary_{}.pdf'.format(i)))
 
     # plot traces:
@@ -162,13 +190,9 @@ for i, group in enumerate(groups):
     fig.savefig(os.path.join(fig_folder, 'traces_{}.pdf'.format(i)))
 
 # save combined for DDM fitting:
-groups = [[4], [7], [10], [13],]
+groups = [[4], [7], [10], [13], [19], [22], [25], [28],]
 for i, group in enumerate(groups): 
-    df_neutral = pd.read_csv(os.path.join(data_folder, 'df_{}.csv'.format(1)))
-    df_neutral.loc[:,'condition'] = 0
     df = pd.concat([pd.read_csv(os.path.join(data_folder, 'df_{}.csv'.format(g))) for g in group], axis=0)
-    df.loc[:,'condition'] = 1
-    df_combined = pd.concat((df_neutral, df))
-    df_combined.loc[:,'subj_idx'] = 0
-    df_combined.loc[:,'rt'] = df_combined.loc[:,'rt'] / 200
-    df_combined.to_csv(os.path.join(data_folder, '2018_lca_data_{}.csv'.format(i+1)))
+    df.loc[:,'condition'] = 0
+    df.loc[:,'subj_idx'] = 0
+    df.to_csv(os.path.join(os.path.expanduser('~/Desktop/simulations/ddm_fits_data/'), '2018_lca_data_{}.csv'.format(i+1)))
